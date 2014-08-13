f8993ea357aa2c7822e7fadd610ff45f6bb08b8d
Max Kim <mminkim89@gmail.com>
2014-08-13 16:27:33 -0700

    failupdate
    Nothing is working properly; try again from scratch later

--- DFS.rb
+++ DFS.rb
@@ -1,3 +1,15 @@
+=begin
+Code is one giant mess; origin of mess started
+with a badly defined node/vertex.
+Need a Data Structure for vertex that is more
+universal.
+
+DO NOT RUN CODE - unless you want it to crash several times
+=end
+require_relative 'graphsDS2'
+
+$count = 0
+
 class DFSnode
 	attr_accessor :value, :color, :source, 
 				  :discover, :finish
@@ -11,12 +23,42 @@ class DFSnode
 	end
 end
 
-class DFStree
-	#root
-	#children = {hash}
+
+def DFS(g)
+	gpi = Graph.new
+	g.edges.each_pair do |v, l|
+		l.each {|w| gpi.addEdge(DFSnode.new(v), DFSnode.new(w))}
+	end
+	$count = 0
+	
+	gpi.vertexList.each{|u| DFS_VISIT(g, gpi, u) if u.color == :WHITE}
+	
+	gpi
 end
 
-class DFSforest
-	#multiple starting, disjoint
+def DFS_VISIT(g, u)
+	$count++
+	u.d = $count
+	u.color = :GRAY
+	
+	g.adj(u).each do |v|
+		if v.color == :WHITE
+			v.source = u
+			g.defineEdge([u,v], :TREEEDGE)
+			DFS_VISIT(g, v)
+		elsif v.color == :GRAY
+			g.defineEdge([u,v], :BACKEDGE)
+		else
+			if u.distance > v.distance
+				g.defineEdge([u,v], :FORWARDEDGE)
+			else
+				g.defineEdge([u,v], :CROSSEDGE)
+			end
+		end
+	end
+	
+	u.color = :BLACK
+	$count++
+	u.finish = $count
 end
 
--- graphsDS2.rb
+++ graphsDS2.rb
@@ -2,6 +2,11 @@
 Note: Implementation of code seen here are used
 for personal educational purposes and do not demonstrate
 any fully practical or optimal solution.
+
+Evaluation: badly designed datastructure; does not
+accommade for future changes or different search structures. 
+Need a data structure base that is more versatile with
+a strong foundation that is universal.
 =end
 
 
@@ -9,16 +14,21 @@ any fully practical or optimal solution.
 #of graphsDS.rb; utilizes Sets and Hashes
 #to basically make everything dumb easy.
 
+
+# TO ADD: Removal of edges and vertices
 require 'set'
 
 class Graph
-	attr_accessor :vertices, :edges
+	attr_reader :vertices, :edges, :edgesSpecial
 	#vertices      :: SET of all vertices
 	#edges         :: HASH where key is vertex from
 	#				  vertexList and value is a set of other vertices
 	def initialize
 		@vertices = Set.new
-		@edges    = Hash.new(Set.new)
+		@edges    = Hash.new() #Using @edges = Hash.new(Set.new) not working
+		@edgesSpecial = Hash.new() #Used for defining values to edges
+								   #Keys = 2 index arrays [u,v]
+								   #Values = type
 	end
 	
 	#Add single vertex to @vertices
@@ -32,6 +42,7 @@ class Graph
 	def addEdge(u, v)
 		@vertices.add(u)
 		@vertices.add(v)
+		@edges[u] = Set.new if !@edges.key?(u)
 		@edges[u].add(v)
 	end
 	
@@ -39,18 +50,39 @@ class Graph
 	#enumerable
 	def addEdges(u, enum)
 		@vertices.add(u)
+		@edges[u] = Set.new if !@edges.key?(u)
 		enum.each do |v|
 			@vertices.add(v)
 			@edges[u].add(v)
 		end
 	end
 	
-	#Returns set of all adjacent vertices
+	#Returns a vertex LIST
+	def vertexList
+		return @vertices.to_a
+	end
+	
+	def defineEdge(e, type)
+		@edgesSpecial[e] = type
+	end
+	
+	#Returns list of all adjacent vertices
 	#to a given vertex u.
 	#If vertex u does not exist or u exists
 	#but does not have any adjacent, returns
-	#empty set
+	#empty list
 	def adj(u)
-		@edges[u]
+		@edges[u].to_a
 	end
 end
+
+=begin
+g = Graph.new
+g.addEdges(5,[1,2,3,4])
+g.addEdge(1,2)
+
+p g.adj(5)
+p g.adj(1)
+p g.adj(2)
+p g.vertexList
+=end
